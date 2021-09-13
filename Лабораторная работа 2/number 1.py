28fb77eb34785c5ab9495b8a60c9f1deea5459c8
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2021-09-13 18:50:22 +0300

    Лабораторная работа 1 готова, положено начало 2 лабе

---
+++ Лабораторная работа 2/number 1.py
@@ -0,0 +1,48 @@
+'''
+Вычислите сумму ряда
+'''
+from math import fabs, factorial, pi
+
+
+def first_example(x, N):
+    if fabs(x) > 1:
+        suma = 0
+        for n in range(N+1):
+            suma += 1/( (2 * n + 1) * x ** (2 * n + 1) )
+        return 2*suma
+    else:
+        return 'Внимание, по условию задачи |x| > 1'
+
+
+def second_example(x, N):
+    suma = 0
+    for n in range(N+1):
+        suma += ((-1)**n * x**n) / (factorial(n))
+    return suma
+
+
+def third_example(x, N):
+    suma = 0
+    for n in range(N+1):
+        suma += ( (-1)**n * x**(2*n) ) / (factorial(2*n))
+    return suma
+
+
+def fourth_example(x, N):
+    suma = pi/2
+    if x > 1:
+        for n in range(N+1):
+            suma += ( (-1)**(n+1) ) / ( (2*n+1) * x**(2*n+1) )
+        return suma
+    else:
+        return 'Внимание, по условию задачи x > 1'
+
+
+def fift_exemple(x, N):
+    if fabs(x) <= 1:
+        suma = 0
+        for n in range(N+1):
+            suma += ( (-1)**n ) / ( (2*n+1) * x**(2*n+1) )
+        return 2*suma
+    else:
+        return 'Внимание, по условию задачи |x| <= 1'
